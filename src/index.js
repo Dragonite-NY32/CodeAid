5a79208ec222bacbeecf71f2ec60c09a4d0c2081
truszala <truszala@gmail.com>
2022-05-23 16:01:08 -0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author
+ * @date
+ * @description entry point for application. Hangs React app off of #contents in index.html
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './store';
+console.log('redux store: ', store.getState());
+render(
+  // wrap the App in the Provider Component and pass in the store
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#contents')
+);
